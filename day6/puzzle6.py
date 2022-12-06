7336f0333255c0dbb0f6bf8cb08df10271b6f9d6
Peter <panatale1@gmail.com>
2022-12-06 08:56:08 -0500

    updates to use function instead of duplicating code

--- day6/puzzle6.py
+++ day6/puzzle6.py
@@ -1,34 +1,21 @@
+def find_marker(signal, marker_len):
+    found = False
+    index = 0
+    for i in range(len(signal)):
+        for j in range(marker_len):
+            if signal[i:i + marker_len].count(signal[i:i + marker_len][j]) > 1:
+                break
+        else:
+            found = True
+            index = i + marker_len
+        if found:
+            break
+    print(index)
+
 with open('input1', 'r') as input_data:
     lines = input_data.readlines()
 
 signal = lines[0].strip()
 
-found = False
-index = 0
-
-for i in range(len(signal)):
-    for j in range(4):
-        if signal[i:i+4].count(signal[i:i+4][j]) > 1:
-            # Definitely not the marker
-            break
-    else:
-        found = True
-        index = i + 4
-    if found:
-        break
-print(index)
-
-found = False
-index = 0
-
-for i in range(len(signal)):
-    for j in range(14):
-        if signal[i:i+14].count(signal[i:i+14][j]) > 1:
-            # Definitely not the marker
-            break
-    else:
-        found = True
-        index = i + 14
-    if found:
-        break
-print(index)
+find_marker(signal, 4)
+find_marker(signal, 14)
