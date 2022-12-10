bd4414da5b86776b5d09fac1b5e2f12a91d1781f
Peter <panatale1@gmail.com>
2022-12-10 08:36:33 -0500

    day 10

---
+++ day10/puzzle10.py
@@ -0,0 +1,49 @@
+from pprint import pprint
+
+with open('input1', 'r') as input_data:
+    commands = input_data.readlines()
+
+X = 1
+cycle = 0
+interesting_cycles = [20, 60, 100, 140, 180, 220]
+signal_strengths = 0
+
+for line in commands:
+    command = line.strip()
+    if command == 'noop':
+        cycle += 1
+        if cycle in interesting_cycles:
+            signal_strengths += X * cycle
+    else:
+        reg = int(command.split()[1])
+        for i in range(2):
+            cycle += 1
+            if cycle in interesting_cycles:
+                signal_strengths += X * cycle
+        else:
+            X += reg
+print(signal_strengths)
+
+screen = [[' ' for i in range(40)] for j in range(6)]
+X = 1
+cycle = 0
+for line in commands:
+    command = line.strip()
+    if command == 'noop':
+        if cycle % 40 in [X - 1, X, X + 1]:
+            row = int(cycle/40)
+            index = cycle % 40
+            screen[row][index] = '#'
+        cycle += 1
+    else:
+        reg = int(command.split()[1])
+        for i in range(2):
+            if cycle % 40 in [X - 1, X, X + 1]:
+                row = int(cycle/40)
+                index = cycle % 40
+                screen[row][index] = '#'
+            cycle += 1
+        else:
+            X += reg
+for i in screen:
+    print(''.join(i))
