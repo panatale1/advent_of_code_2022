a478cc5cfa4d38d6b1c7953385afc8dc630491c1
Peter <panatale1@gmail.com>
2022-12-09 22:42:52 -0500

    day 9

--- day9/puzzle9.py
+++ day9/puzzle9.py
@@ -56,43 +56,39 @@ for line in directions:
 print(len(tail_visited))
 
 def knot_moves(head, tail, direction):
-    if direction == 'R':
-        if head[1] - tail[1] == 0 and head[0] - tail[0] > 1:
-            tail[0] += 1
-        elif head[1] - tail[1] != 0 and head[0] - tail[0] > 1:
-            tail[0] += 1
-            if head[1] - tail[1] > 0:
-                tail[1] += 1
-            else:
-                tail[1] -= 1
-    elif direction == 'L':
-        if head[1] - tail[1] == 0 and head[0] - tail[0] < -1:
-            tail[0] -= 1
-        elif head[1] - tail[1] != 0 and head[0] - tail[0] < -1:
-            tail[0] -= 1
-            if head[1] - tail[1] > 0:
-                tail[1] += 1
-            else:
-                tail[1] -= 1
-    elif direction == 'U':
-        if head[0] - tail[0] == 0 and head[1] - tail[1] > 1:
+    if head[1] - tail[1] == 0 and head[0] - tail[0] > 1:
+        tail[0] += 1
+    elif head[1] - tail[1] != 0 and head[0] - tail[0] > 1:
+        tail[0] += 1
+        if head[1] - tail[1] > 0:
             tail[1] += 1
-        elif head[0] - tail[0] != 0 and head[1] - tail[1] > 1:
+        else:
+            tail[1] -= 1
+    elif head[1] - tail[1] == 0 and head[0] - tail[0] < -1:
+        tail[0] -= 1
+    elif head[1] - tail[1] != 0 and head[0] - tail[0] < -1:
+        tail[0] -= 1
+        if head[1] - tail[1] > 0:
             tail[1] += 1
-            if head[0] - tail[0] > 0:
-                tail[0] += 1
-            else:
-                tail[0] -= 1
-    elif direction == 'D':
-        if head[0] - tail[0] == 0 and head[1] - tail[1] < -1:
+        else:
             tail[1] -= 1
-        elif head[0] - tail[0] != 0 and head[1] - tail[1] < -1:
-            tail[1] -= 1
-            if head[0] - tail[0] > 0:
-                tail[0] += 1
-            else:
-                tail[0] -= 1
-    
+    elif head[0] - tail[0] == 0 and head[1] - tail[1] < -1:
+        tail[1] -= 1
+    elif head[0] - tail[0] != 0 and head[1] - tail[1] < -1:
+        tail[1] -= 1
+        if head[0] - tail[0] > 0:
+            tail[0] += 1
+        else:
+            tail[0] -= 1
+    elif head[0] - tail[0] == 0 and head[1] - tail[1] > 1:
+        tail[1] += 1
+    elif head[0] - tail[0] != 0 and head[1] - tail[1] > 1:
+        tail[1] += 1
+        if head[0] - tail[0] > 0:
+            tail[0] += 1
+        else:
+            tail[0] -= 1
+        
 tail_visited = [[0,0]]
 rope_dict = {i:[0,0] for i in range(10)}
 for line in directions:
